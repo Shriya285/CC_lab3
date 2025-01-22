c7de5782506103530ec8fd73347c8a4d3b775653
Shriya285 <116240717+Shriya285@users.noreply.github.com>
2025-01-22 23:48:33 +0530

    Update get-cart-locustfile.py

--- locust/get-cart-locustfile.py
+++ locust/get-cart-locustfile.py
@@ -1,45 +1,32 @@
+
 from locust import task, run_single_user
 from locust import FastHttpUser
 from insert_product import login
 
-class add_to_cart(FastHttpUser):
+class AddToCart(FastHttpUser):
     def __init__(self, environment):
         super().__init__(environment)
-        self.username="test123"
-        self.password="test123"
-        cookies=login(self.username, self.password)
-        self.token=cookies.get("token")
-    host = "http://localhost:5000"
-    default_headers = {
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Connection": "keep-alive",
-        "DNT": "1",
-        "Sec-GPC": "1",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
-    }
+        self.username = "test123"
+        self.password = "test123"
+        cookies = login(self.username, self.password)
+        self.token = cookies.get("token")
+        self.host = "http://localhost:5000"
+        self.default_headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
+            "Cookies": f"token:{self.token}",
+        }
 
     @task
-    def t(self):
-        with self.client.request(
-            "GET",
-            "/cart",
-            headers={
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
-                "Cookies": "token:{}".format(self.token),
-                "Host": "localhost:5000",
-                "Priority": "u=0, i",
-                "Referer": "http://localhost:5000/product/1",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "same-origin",
-                "Sec-Fetch-User": "?1",
-                "Upgrade-Insecure-Requests": "1",
-            },
-            catch_response=True,
-        ) as resp:
-            pass
-
+    def view_cart(self):
+        with self.client.get(
+            "/cart", 
+            headers=self.default_headers, 
+            catch_response=True
+        ) as response:
+            if response.status_code == 200:
+                response.success()
+            else:
+                response.failure(f"Failed with status code: {response.status_code}")
 
 if __name__ == "__main__":
-    run_single_user(add_to_cart)
+    run_single_user(AddToCart)
